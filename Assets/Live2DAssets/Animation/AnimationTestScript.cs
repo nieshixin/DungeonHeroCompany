270246bb015b5878b741959550ab791213d6e714
JerryNie <nieshixin@gmail.com>
2017-11-06 21:03:11 -0500

    Basic animation controller test setup, anim state transition test complete

---
+++ Assets/Live2DAssets/Animation/AnimationTestScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTestScript : MonoBehaviour {
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = gameObject.GetComponent<Animator> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A)) {
+			if (anim.GetBool ("Cry")) {
+				anim.SetBool ("Cry", false);
+			}
+			else if (anim.GetBool ("Cry") == false) {
+				anim.SetBool ("Cry", true);
+			}
+		}
+		
+	}
+}
