e7c49a16b6f0ca44b8c9d39b4a938220c95d6e94
JerryNie <nieshixin@gmail.com>
2017-11-09 00:20:28 -0500

    Setup Mockup level added UI animation  UI animator controller imported blur shader

---
+++ Assets/DungeonHeroCompany/Scripts/UIscripts/MailListBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MailListBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	Animator anim;
+
+	void Start () {
+		anim = GetComponent<Animator> (); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ShowMailList(){
+		anim.SetBool ("Show", true);
+	}
+	public void HideMailList(){
+		anim.SetBool ("Show", false);
+	}
+}
